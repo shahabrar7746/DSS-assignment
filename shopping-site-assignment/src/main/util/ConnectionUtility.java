0802ae6b6296f4b68ca63407df074a857ca898f6
shahabrar7746 <shahabrar7746@gmail.com>
2025-03-04 17:10:44 +0530

    worked on updates

--- shopping-site-assignment/src/main/util/ConnectionUtility.java
+++ shopping-site-assignment/src/main/util/ConnectionUtility.java
@@ -1,38 +1,37 @@
 package main.util;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 import java.util.Scanner;
 
 public final class ConnectionUtility {
-private ConnectionUtility(){}
-    private static Connection con;
-    public static Connection getConnection() { // TODO
+    private ConnectionUtility() {
+    }
 
-        if(con == null) {
+    private static Connection con;
+
+    public static Connection getConnection() {
+
+        if (con == null) {
             try {
-                String[] credentials = getDataBaseCredentials();
-                String url = credentials[0];
-                String user = credentials[1];
-                String password = credentials[2];
+                Properties properties = new Properties();
+                FileInputStream fileInputStream = new FileInputStream("src/config.properties");
+                properties.load(fileInputStream);
+                String url = properties.getProperty("database.url");
+                String user = properties.getProperty("database.username");
+                String password = properties.getProperty("database.password");
                 con = DriverManager.getConnection(url, user, password);
-} catch (FileNotFoundException | SQLException ex ) {
+            } catch (SQLException | IOException ex) {
                 System.out.println("Properties file not found");
             }
         }
         return con;
     }
-    private static String[] getDataBaseCredentials() throws FileNotFoundException {
-        File file = new File("src/properties");
-        Scanner scanner = new Scanner(file);
-        String[] arr= new String[3];
-        int index = 0;
-        while(scanner.hasNextLine()){
-            arr[index++] = scanner.nextLine().split("=")[1];
-        }
-        return arr;
-    }
+
 }
